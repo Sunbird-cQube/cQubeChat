ba661759c3d6a12e87a1e79716b77c404cb8e43c
deepanshhuuu <deepanshu27082002@gmail.com>
2023-06-19 23:53:52 +0530

    Added OpenAI API keys, LangChain libraries, and improved UI

--- cQubeChat.py
+++ cQubeChat.py
@@ -1,4 +1,6 @@
+import streamlit.components.v1 as components
 import streamlit as st
+from dotenv import load_dotenv
 from streamlit_chat import message
 from streamlit_searchbox import st_searchbox
 from utils.prompt_manager import PromptManager
@@ -14,6 +16,11 @@ import os
 import requests
 import json
 import pandas as pd
+from langchain.schema import(
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
 
 # os.environ['OPENAI_API_KEY'] = "your_open_api_key"
 # os.environ['SERPAPI_API_KEY'] = "your_serp_api_key"
@@ -36,48 +43,77 @@ import pandas as pd
 #     verbose = True
 # )
 
-st.set_page_config(
-    page_title="cQubeChat",
-    layout="centered",
-    initial_sidebar_state="auto",
-    menu_items={
-        'Report a bug': "https://github.com/Sunbird-cQube/cQubeChat",
-        'About': 
-            '''
-            cQubeChat is an intuitive and user-friendly application that allows you to interact with your cQube data using natural language prompts. It is built using OpenAI's GPT-4 and Streamlit. 
-            Go to the GitHub repo to learn more about the project. https://github.com/Sunbird-cQube/cQubeChat
-            '''
-    }
-)
-
-st.title("cQubeChat")
-st.caption("Talk your way through data")
-
-# Set up session state
-# To store the chat history
-if 'chat_history' not in st.session_state:
-    st.session_state.chat_history = []
-
-# User input text box
-user_input = st.text_input("Search yout Query here !")
-
-# Process user input and display chat history
-if st.button("Send"):
-    # Add user input to chat history
-    st.session_state.chat_history.append(("User", user_input))
-    
-    # Get chatbot response
-    bot_response = agent_executor.run(user_input)
-    
-    # Add bot response to chat history
-    st.session_state.chat_history.append(("Chatbot", bot_response))
-    
-# Display chat history
-for sender, message in st.session_state.chat_history:
-    if sender == "User":
-        st.text_input("User:", value=message, disabled=True)
+def init():
+    load_dotenv()
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPEN_API_KEY") == "":
+        print("OPEN_API_KEY is not set")
+        exit(1)
     else:
-        st.text_area("Chatbot:", value=message, disabled=True)
+        print("OPEN_API_KEY is set")
+
+    st.set_page_config(page_title="cQubeChat", page_icon=":robot_face:", layout="centered")
+    
+
+def main():
+    init() 
+    
+    with st.sidebar:
+        st.header("Talk your way through data")
+        user_input = st.text_input("Search your Query :", key = "user_input")
+
+    st.title(":robot_face: cQubeChat")
+ 
+    with open("ui/sidebar.md", "r") as sidebar_file:
+       sidebar_content = sidebar_file.read()
+
+    with open("ui/styles.md", "r") as styles_file:
+         styles_content = styles_file.read()
+    
+    # Displays the content in the sidebar  
+    st.sidebar.write(sidebar_content)
+
+    chat = ChatOpenAI(temperature=0)
+    
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            SystemMessage(content=" Hi I'm cQubeChat, an intuitive and user-friendly application that allows you to interact with your CQube data using natural language queries.")
+        ]
+
+    if user_input:
+        st.session_state.messages.append(HumanMessage(content=user_input))
+        with st.spinner("Generating...."):
+            response = chat(st.session_state.messages)
+        st.session_state.messages.append(AIMessage(content=response.content))      
+
+    messages = st.session_state.get('messages', [])
+    for i, msg in enumerate(messages[1:]):
+        # if i == 0:
+        #     message(msg.content, is_user = False, key = str(i) + '_system')
+        if i % 2 == 0:
+            message(msg.content, is_user = True, key=str(i) + '_user')
+        else:
+            message(msg.content, is_user=False, key = str(i) + '_ai')    
+
+
+if __name__ == '__main__':
+    main()   
+#     st.set_page_config(
+#         page_title="cQubeChat",
+#         layout="centered",
+#         initial_sidebar_state="auto",
+#         menu_items={
+#             'Report a bug': "https://github.com/Sunbird-cQube/cQubeChat",
+#             'About': 
+#             '''
+#             cQubeChat is an intuitive and user-friendly application that allows you to interact with your cQube data using natural language prompts. It is built using OpenAI's GPT-4 and Streamlit. 
+#             Go to the GitHub repo to learn more about the project. https://github.com/Sunbird-cQube/cQubeChat
+#             '''
+#             }
+#     )
+
+# st.title("cQubeChat")
+# st.caption("Talk your way through data")
+
 # # Adding keys to store the chat
 # if 'generated' not in st.session_state:
 #     st.session_state['generated'] = []
@@ -85,15 +121,11 @@ for sender, message in st.session_state.chat_history:
 # if 'past' not in st.session_state:
 #     st.session_state['past'] = []
 
-def add_sidebar():
-    with open("ui/sidebar.md", "r") as sidebar_file:
-      sidebar_content = sidebar_file.read()
+# if 'user_input' not in st.session_state:
+#     st.session_state['user_input'] = ''
 
-    with open("ui/styles.md", "r") as styles_file:
-      styles_content = styles_file.read()
+# def add_sidebar():
     
-    # Displays the content in the sidebar  
-    st.sidebar.write(sidebar_content)
 
 # def add_body(search_counter = 0):
 #     c = st.container()
@@ -105,41 +137,40 @@ def add_sidebar():
 #     # Placeholder for the text input
 #     prompt_manager = PromptManager()
 
-    # #Adding a searchbox
-    # user_input = st_searchbox(
-    #     search_function=prompt_manager.search,
-    #     placeholder="Start Typing",
-    #     label="",
-    #     default="",
-    #     clear_on_submit=False,
-    #     clearable=True,
-    # )
-
-    # Submit button placeholder
-    # submit_button_placeholder = st.empty()
-    # # Save the typed input in session state
-    # st.session_state['user_input'] = user_input
-    # User input text box
-
-#Define a button for submitting the query
-    # submit_button = submit_button_placeholder.button("Submit Query")
+#     user_input = st_searchbox(
+#         search_function=prompt_manager.search,
+#         placeholder="Start Typing",
+#         label="",
+#         default="",
+#         clear_on_submit=False,
+#         clearable=True
+#     )
+#     #Submit button placeholder
+#     submit_button_placeholder = st.empty()
+#     # Save the typed input in session state
+#     st.session_state['user_input'] = user_input
+#     #User input text box
     
-    # if submit_button and st.session_state["searchbox"]["result"]!='':
+#     #Define a button for submitting the query
+#     submit_button = submit_button_placeholder.button("Submit Query")
+    
+#     if submit_button and st.session_state["searchbox"]["result"]!='':
 
-    #     print("Will Send input to Text2SQL : " + user_input)
-    #     # TODO: register callback for Text2SQL 
         
-    #     # TODO: store the output 
-    #     st.session_state.past.append(user_input)
-    #     st.session_state.generated.append("Will Send input to Text2SQL : " + user_input)
+#         print("Will Send input to Text2SQL : " + user_input)
+#         # TODO: register callback for Text2SQL 
         
-    #     # Empty the user_input session state
-    #     st.session_state['user_input'] = ''
-    #     st.session_state["searchbox"]["result"]=''
+#         # TODO: store the output 
+#         st.session_state.past.append(user_input)
+#         st.session_state.generated.append("Will Send input to Text2SQL : " + user_input)
+        
+#         # Empty the user_input session state
+#         st.session_state['user_input'] = ''
+#         st.session_state["searchbox"]["result"]=''
 
-    #     # Force a rerun to immediately reflect the changes in the chat
-    #     st.experimental_rerun()
+#         # Force a rerun to immediately reflect the changes in the chat
+#         # st.experimental_rerun()
 
-# Function calls
-add_sidebar()
+# #Function calls
+# add_sidebar()
 # add_body()
